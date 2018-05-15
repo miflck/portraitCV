54389e027208072880487500ad98452f4b4c6c61
miflck <contact@michaelflueckiger.ch>
2018-05-15 20:20:39 +0200

    settings

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 //========================================================================
 int main( ){
 	//ofSetupOpenGL(1924,1200,OF_WINDOW);			// <-------- setup the GL context
-ofSetupOpenGL(1366,768,OF_FULLSCREEN);
+ofSetupOpenGL(720,480,OF_FULLSCREEN);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
